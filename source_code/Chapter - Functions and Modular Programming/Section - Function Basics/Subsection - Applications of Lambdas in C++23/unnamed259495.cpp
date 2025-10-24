65186971ea5b614289ed28c45205a207cae324f6
BurstBooks <administrator@burstbooks.biz>
2025-10-24 16:15:19 +1300

    Upload source_code/Chapter - Functions and Modular Programming/Section - Function Basics/Subsection - Applications of Lambdas in C++23/unnamed259495.cpp

---
+++ source_code/Chapter - Functions and Modular Programming/Section - Function Basics/Subsection - Applications of Lambdas in C++23/unnamed259495.cpp
@@ -0,0 +1,10 @@
+#include 
+#include 
+#include 
+
+int main() {
+    std::vector> data = {
+        {5, "apple"}, {3, "banana"}, {5, "cherry"}, {2, "date"}
+    };
+    // Write your lambda here and sort 'data'
+}
